5226cf0d8766904fcd7b16b1ff89602243e05343
Tomasz Grzesiak <grzesiak.t@gmail.com>
2015-01-05 13:09:21 +0100

    Added solutions to topic 1 - ex1, ex2

--- 1-threads-basics/students/src/exercise2/MyThread.java
+++ 1-threads-basics/students/src/exercise2/MyThread.java
@@ -1,4 +1,24 @@
 package exercise2;
 
-public class MyThread {
+public class MyThread extends Thread {
+
+    public MyThread(String name){
+        super(name);
+
+    }
+
+    public void run(){
+
+        for(int i=1;i<=10;i++){
+
+            System.out.println("Thread " + this.getName() + ": iteration " + i);
+            try {
+                Thread.sleep(10);
+            }
+            catch(Exception e){
+                System.out.println("Something bad happened");
+                e.printStackTrace();
+            }
+        }
+    }
 }
