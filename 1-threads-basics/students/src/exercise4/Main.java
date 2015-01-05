29725a69fdd832d691670206420b34a9041752c2
Tomasz Grzesiak <grzesiak.t@gmail.com>
2015-01-05 13:55:03 +0100

    Added solutions to topic 1 - ex4, ex4

--- 1-threads-basics/students/src/exercise4/Main.java
+++ 1-threads-basics/students/src/exercise4/Main.java
@@ -1,8 +1,25 @@
 package exercise4;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for(int i=0;i<4;i++){
+
+            executorService.execute(new MyRunnable());
+        }
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("FINISHED");
     }
 }
--- 1-threads-basics/students/src/exercise4/MyRunnable.java
+++ 1-threads-basics/students/src/exercise4/MyRunnable.java
@@ -1,4 +1,15 @@
 package exercise4;
 
-public class MyRunnable {
+import java.util.concurrent.TimeUnit;
+
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+
+        for(int i=0;i<10;i++) {
+
+            System.out.println("Thread: " + Thread.currentThread().getName() + " Iteration: " + i);
+        }
+    }
 }
